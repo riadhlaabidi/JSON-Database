dcec5b04deed45eb15117e58d4a7c0db14dd8602
riadh26 <riadh.by.laabidi@gmail.com>
2021-03-30 05:18:10 +0100

    expose fields for serialization

--- src/main/java/server/cli/CommandLineArgs.java
+++ src/main/java/server/cli/CommandLineArgs.java
@@ -1,23 +1,28 @@
 package server.cli;
 
 import com.beust.jcommander.Parameter;
+import com.google.gson.annotations.Expose;
 
 public class CommandLineArgs {
+
+    @Expose
     @Parameter(
             names = {"-t", "--type"},
             description = "Type of the request",
             order = 0)
     public String type;
 
+    @Expose
     @Parameter(
             names = {"-k", "--key"},
             description = "Record key",
             order = 1)
     public String key;
 
+    @Expose
     @Parameter(
             names = {"-v", "--value"},
-            description = "Text value to add",
+            description = "Value to add",
             order = 2)
     public String value;
 
