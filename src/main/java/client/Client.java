fa359650f7b3a99d857ea11d1b8412afd82e093b
riadh26 <riadh.by.laabidi@gmail.com>
2021-03-30 05:12:43 +0100

    add data dir path and modify request

--- src/main/java/client/Client.java
+++ src/main/java/client/Client.java
@@ -1,15 +1,28 @@
 package client;
 
 import server.cli.CommandLineArgs;
-import server.requests.Request;
+import server.utils.GsonUtils;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class Client {
+
+    private static final Path DATA_DIR_PATH = Paths.get(
+            "src" + File.separator +
+            "main" + File.separator +
+            "java" + File.separator +
+            "client" + File.separator +
+            "data").toAbsolutePath();
+
     private static final String ADDRESS = "localhost";
     private static final int PORT = 9000;
 
@@ -21,11 +34,17 @@ public class Client {
                 DataInputStream input = new DataInputStream(socket.getInputStream());
                 DataOutputStream output = new DataOutputStream(socket.getOutputStream())
         ) {
-            Request request = new Request(cla);
-            output.writeUTF(request.toJson());
-            System.out.println("Sent: " + request.toJson());
+
+            String request = cla.filename != null
+                    ? new String(Files.readAllBytes(DATA_DIR_PATH.resolve(cla.filename)))
+                    : GsonUtils.prettyGson.toJson(cla);
+
+            output.writeUTF(request);
+            System.out.println("Sent: " + request);
             System.out.println("Received: " + input.readUTF());
-        } catch (IOException e) {
+        } catch (NoSuchFileException e) {
+            System.out.println("Cannot read file: " + e.getMessage());
+        }catch (IOException e) {
             e.printStackTrace();
         }
     }
